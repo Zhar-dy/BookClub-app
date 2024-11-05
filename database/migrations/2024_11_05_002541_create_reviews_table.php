b71a0b9b653a883a9c89ac13099b271970ffe3a0
Zhardy <zhardydev@gmail.com>
2024-11-05 08:44:04 +0800

    first commit

---
+++ database/migrations/2024_11_05_002541_create_reviews_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained('users');
+            $table->foreignId('book_id')->nullable()->constrained('books');
+            $table->tinyInteger('rating')->nullable(); // 0 = Amazing, 1 = Good, 2 = Bad
+            $table->string('comment')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
